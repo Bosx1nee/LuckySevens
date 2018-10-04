af5869c37825dc8c16def2f1586e47fd6514d62e
Your Name <you@example.com>
2018-10-04 15:02:25 -0600

    added files on staged

--- Luckysevens/Program.cs
+++ Luckysevens/Program.cs
@@ -6,7 +6,22 @@ namespace Luckysevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalNumberofSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <=100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                    totalNumberofSevens++;
+            }
+            Console.WriteLine("Out of 100, you rolled {0} sevens", totalNumberofSevens);
+            Console.ReadLine();
         }
     }
 }
